d1250e512827445d9ec6449812834906893f88ad
vicanso <tree.xie@outlook.com>
2026-02-08 09:19:33 +0800

    style(ui): improve windows icon clarity

--- build.rs
+++ build.rs
@@ -14,6 +14,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut res = winres::WindowsResource::new();
 
         res.set_icon("icons/zedis.ico");
+        res.set_manifest(
+            r#"
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <asmv3:application xmlns:asmv3="urn:schemas-microsoft-com:asm.v3">
+            <asmv3:windowsSettings>
+                <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/pm</dpiAware>
+                <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+            </asmv3:windowsSettings>
+        </asmv3:application>
+        </assembly>
+        "#,
+        );
 
         if let Err(e) = res.compile() {
             eprintln!("Failed to compile Windows resources: {}", e);
